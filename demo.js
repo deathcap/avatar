8a3cd296e42697d3b44abf5fa11b996c2cb9597d
deathcap <deathcap@gmx.co.uk>
2014-04-21 18:38:27 -0700

    Add orbit camera
    Integrated based on
    https://github.com/hughsk/indexed-geometry-demo/blob/master/index.js

--- demo.js
+++ demo.js
@@ -1,12 +1,19 @@
+'use strict';
 
 var createShell = require('gl-now');
+var createCamera = require('game-shell-orbit-camera');
 var glslify = require('glslify');
+var glm = require('gl-matrix');
+var mat4 = glm.mat4;
 
-shell = createShell();
+var shell = createShell();
 var shader;
+var camera;
 
 shell.on("gl-init", function() {
   var gl = shell.gl
+
+  camera = createCamera(shell);
 
   shader = glslify({vertex: './avatar.vert', fragment: './avatar.frag'})(gl);
 
@@ -24,14 +31,20 @@ shell.on("gl-init", function() {
   gl.vertexAttribPointer(position_attribute, 3, gl.FLOAT, false, 0, 0)
 })
 
+var view = mat4.create();
+var proj = mat4.create();
+
 shell.on("gl-render", function(t) {
   var gl = shell.gl
 
+  camera.view(view);
+  mat4.perspective(proj, Math.PI / 4, shell.width / shell.height, 0.001, 1000);
+  mat4.mul(proj, proj, view);
+
   shader.bind();
+  shader.uniforms.matrix = view;
   shader.attributes.position.location = 0;
   gl.drawArrays(gl.TRIANGLES, 0, 3)
 })
 
-shell.on("gl-error", function(e) {
-  throw new Error("WebGL not supported :(")
-})
+
