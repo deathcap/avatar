1ad504a8338dd3fcb4f3c2576beb24de6189f9e6
deathcap <deathcap@gmx.co.uk>
2014-04-21 18:35:47 -0700

    Switch to using glslify

--- avatar.vert
+++ avatar.vert
@@ -1,7 +1,8 @@
 attribute vec3 position;
-uniform mat4 matrix;
+//uniform mat4 matrix;
 
 void main() {
-    gl_Position = matrix * vec4(position, 1.0);
+    //gl_Position = matrix * vec4(position, 1.0);
+    gl_Position = vec4(position, 1.0);
 }
 
--- demo.js
+++ demo.js
@@ -1,32 +1,14 @@
-//Initialize shell
-var shell = require("gl-now")()
+
+var createShell = require('gl-now');
+var glslify = require('glslify');
+
+shell = createShell();
+var shader;
 
 shell.on("gl-init", function() {
   var gl = shell.gl
 
-  //Create fragment shader
-  var fs = gl.createShader(gl.FRAGMENT_SHADER)
-  gl.shaderSource(fs, [
-    "void main() {",
-      "gl_FragColor = vec4(1, 0, 0, 1);",
-    "}"].join("\n"))
-  gl.compileShader(fs)
-
-  //Create vertex shader
-  var vs = gl.createShader(gl.VERTEX_SHADER)
-  gl.shaderSource(vs, [
-    "attribute vec3 position;",
-    "void main() {",
-      "gl_Position = vec4(position, 1.0);",
-    "}"].join("\n"))
-  gl.compileShader(vs)
-
-  //Link
-  var shader = gl.createProgram()
-  gl.attachShader(shader, fs)
-  gl.attachShader(shader, vs)
-  gl.linkProgram(shader)
-  gl.useProgram(shader)
+  shader = glslify({vertex: './avatar.vert', fragment: './avatar.frag'})(gl);
 
   //Create buffer
   gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer())
@@ -37,7 +19,7 @@ shell.on("gl-init", function() {
   ]), gl.STATIC_DRAW)
 
   //Set up attribute pointer
-  var position_attribute = gl.getAttribLocation(shader, "position")
+  var position_attribute = gl.getAttribLocation(shader.handle, "position")
   gl.enableVertexAttribArray(position_attribute)
   gl.vertexAttribPointer(position_attribute, 3, gl.FLOAT, false, 0, 0)
 })
@@ -45,7 +27,8 @@ shell.on("gl-init", function() {
 shell.on("gl-render", function(t) {
   var gl = shell.gl
 
-  //Draw arrays
+  shader.bind();
+  shader.attributes.position.location = 0;
   gl.drawArrays(gl.TRIANGLES, 0, 3)
 })
 
