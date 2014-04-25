edb5b27ef76945f6c61a80360079bb39b6d3e24d
deathcap <deathcap@gmx.co.uk>
2014-04-24 20:23:37 -0700

    Move generateMesh to avatar.js

--- avatar.js
+++ avatar.js
@@ -1,4 +1,11 @@
-'use strict';
+'use strict'
+
+var createVAO    = require('gl-vao')
+var createBuffer = require('gl-buffer')
+
+var glm          = require('gl-matrix')
+var mat4         = glm.mat4
+var vec3         = glm.vec3
 
 // 64x64 skin format http://i.imgur.com/SnDKuc1.png
 // based on http://www.reddit.com/r/Minecraft/comments/1vd7ue/new_skin_layout_explanation_in_comments/
@@ -21,28 +28,189 @@ var coords = {
   left_leg2:  [ 0, 48, 16, 16],
   left_leg:   [16, 48, 16, 16],
   left_arm:   [32, 48, 16, 16],
-  left_arm2:  [48, 48, 16, 16],
-};
-
-var uvs = {};
-
-// overall size of skin texture TODO: support old 64x32 (clone left/right)
-var total_w = 64;
-var total_h = 64;
-
-for (var name in coords) {
-  var x = coords[name][0], y = coords[name][1], w = coords[name][2], h = coords[name][3];
-
-  var uv = [
-    // top-left
-    x / total_w,
-    y / total_h,
-    (x + w) / total_w,
-    (y + h) / total_h
-    ];
-
-  uvs[name] = uv;
+  left_arm2:  [48, 48, 16, 16]
 }
 
-console.log(uvs);
+var showUVs = function() {
+  var uvs = {}
 
+  // overall size of skin texture TODO: support old 64x32 (clone left/right)
+  var total_w = 64
+  var total_h = 64
+
+  for (var name in coords) {
+    var x = coords[name][0], y = coords[name][1], w = coords[name][2], h = coords[name][3]
+
+    var uv = [
+      // top-left
+      x / total_w,
+      y / total_h,
+      (x + w) / total_w,
+      (y + h) / total_h
+      ]
+
+    uvs[name] = uv
+  }
+
+  console.log(uvs)
+}
+
+var applyTransformToVertices = function(vertices, matrix) {
+  for (var i = 0; i < vertices.length / 3; ++i) {
+    var vertex = vertices.subarray(i * 3, (i + 1) * 3)
+
+    vec3.transformMat4(vertex, vertex, matrix)
+  }
+}
+
+var generateMesh = function(gl) {
+  // Cube coordinates, see https://developer.mozilla.org/en-US/docs/Web/WebGL/Creating_3D_objects_using_WebGL
+  var cube = new Float32Array([
+    // Back face
+    -0.5, -0.5,  0.5,
+     0.5, -0.5,  0.5,
+     0.5,  0.5,  0.5,
+    -0.5,  0.5,  0.5,
+
+    // Front face
+    -0.5, -0.5, -0.5,
+    -0.5,  0.5, -0.5,
+     0.5,  0.5, -0.5,
+     0.5, -0.5, -0.5,
+
+    // Top face
+    -0.5,  0.5, -0.5,
+    -0.5,  0.5,  0.5,
+     0.5,  0.5,  0.5,
+     0.5,  0.5, -0.5,
+
+    // Bottom face
+    -0.5, -0.5, -0.5,
+     0.5, -0.5, -0.5,
+     0.5, -0.5,  0.5,
+    -0.5, -0.5,  0.5,
+
+    // Left face
+     0.5, -0.5, -0.5,
+     0.5,  0.5, -0.5,
+     0.5,  0.5,  0.5,
+     0.5, -0.5,  0.5,
+
+    // Right face
+    -0.5, -0.5, -0.5,
+    -0.5, -0.5,  0.5,
+    -0.5,  0.5,  0.5,
+    -0.5,  0.5, -0.5
+  ])
+
+  var cubeCount = 2
+
+  // add vetices for each cube
+  var verticesArray = new Float32Array(cube.length * cubeCount)
+  for (var i = 0; i < cubeCount; ++i) {
+    var thisCube = new Float32Array(cube.length)
+    thisCube.set(cube)
+
+    if (i == 1) {
+      // test moving over the 2nd cube as an example, to prove it exists
+      // TODO: real transformations, for each body part
+      var matrix = mat4.create()
+      mat4.scale(matrix, matrix, [1.0, 1.5, 0.5])
+      mat4.translate(matrix, matrix, [0, -1.0, 0]) // TODO: fix 1/8 offset Y
+
+      applyTransformToVertices(thisCube, matrix)
+    }
+
+    verticesArray.set(thisCube, cube.length * i)
+  }
+
+  // Create the position buffer.
+  var vertices = createBuffer(gl, verticesArray)
+
+  var cubeVertexIndices = new Uint16Array([
+    0,  1,  2,      0,  2,  3,    // back
+    4,  5,  6,      4,  6,  7,    // front
+    8,  9,  10,     8,  10, 11,   // top
+    12, 13, 14,     12, 14, 15,   // bottom
+    16, 17, 18,     16, 18, 19,   // left
+    20, 21, 22,     20, 22, 23    // right
+  ])
+
+  // repeat vertex indices for each cube, offset by cube vertex count
+  var indexArray = new Uint16Array(cubeVertexIndices.length * cubeCount)
+  for (var i = 0; i < cubeCount; ++i) {
+    for (var j = 0; j < cubeVertexIndices.length; ++j) {
+      indexArray[i * cubeVertexIndices.length + j] = cubeVertexIndices[j] + (cube.length / 3) * i
+    }
+  }
+
+  // Create the index buffer. This is instead packed into
+  // a UInt16Array: note that this is important, otherwise
+  // your model won't render correctly. Also important is
+  // that you label this buffer as an ELEMENT_ARRAY_BUFFER,
+  // or WebGL will hassle you and refuse to draw the VAO.
+  var index = createBuffer(gl
+    , indexArray
+    , gl.ELEMENT_ARRAY_BUFFER
+  )
+
+
+  var uvArray = new Float32Array(2 * 4 * 6 * cubeCount)
+
+  var tw = 64, th = 32
+  var setCubeFaceUV = function(face,x,y,w,h) {
+    w = w || 8
+    h = h || 8
+
+    var i = face * 8
+
+    uvArray[i    ] =  x      / tw
+    uvArray[i + 1] = (y + h) / th
+
+    uvArray[i + 2] =  x      / tw
+    uvArray[i + 3] =  y      / th
+
+    uvArray[i + 4] = (x + w) / tw
+    uvArray[i + 5] =  y      / th
+
+    uvArray[i + 6] = (x + w) / tw
+    uvArray[i + 7] = (y + h) / th
+  }
+
+  // head
+  setCubeFaceUV(0, 24, 8, 8, 8) // back
+  setCubeFaceUV(1,  8, 8, 8, 8) // front
+  setCubeFaceUV(2,  8, 0, 8, 8) // top
+  setCubeFaceUV(3, 16, 0, 8, 8) // bottom
+  setCubeFaceUV(4,  0, 8, 8, 8) // left // TODO: this has to be rotated?
+  setCubeFaceUV(5, 16, 8, 8, 8) // right
+
+  // TODO: body
+  setCubeFaceUV(6, 24, 8, 8, 8) // back
+  setCubeFaceUV(7,  8, 8, 8, 8) // front
+  setCubeFaceUV(8,  8, 0, 8, 8) // top
+  setCubeFaceUV(9, 16, 0, 8, 8) // bottom
+  setCubeFaceUV(10, 0, 8, 8, 8) // left
+  setCubeFaceUV(11,16, 8, 8, 8) // right
+
+
+  var uv = createBuffer(gl, uvArray)
+
+  // Create a VAO from the position buffer, indexed by the
+  // index buffer.
+  var mesh = createVAO(gl, [
+      {
+        buffer: vertices
+        , size: 3
+      },
+      {
+        buffer: uv
+        , size: 2
+      }
+  ], index)
+  mesh.length = indexArray.length
+
+  return mesh
+}
+
+module.exports = generateMesh
--- demo.js
+++ demo.js
@@ -19,13 +19,7 @@ var shell = createShell({
   clearColor: [0, 0, 0, 1]
 })
 
-var applyTransformToVertices = function(vertices, matrix) {
-  for (var i = 0; i < vertices.length / 3; ++i) {
-    var vertex = vertices.subarray(i * 3, (i + 1) * 3)
-
-    vec3.transformMat4(vertex, vertex, matrix)
-  }
-}
+var generateMesh = require('./avatar.js')
 
 var init = function() {
   gl = shell.gl
@@ -41,7 +35,7 @@ var init = function() {
   camera = createCamera(shell)
   camera.distance = 10
 
-  mesh = generateMesh()
+  mesh = generateMesh(gl)
 
   // This super-basic shader is loaded in using glslify, see
   // shader.frag and shader.vert.
@@ -50,156 +44,6 @@ var init = function() {
     , fragment: './avatar.frag'
   })(gl)
 
-}
-
-var generateMesh = function() {
-  // Cube coordinates, see https://developer.mozilla.org/en-US/docs/Web/WebGL/Creating_3D_objects_using_WebGL
-  var cube = new Float32Array([
-    // Back face
-    -0.5, -0.5,  0.5,
-     0.5, -0.5,  0.5,
-     0.5,  0.5,  0.5,
-    -0.5,  0.5,  0.5,
-
-    // Front face
-    -0.5, -0.5, -0.5,
-    -0.5,  0.5, -0.5,
-     0.5,  0.5, -0.5,
-     0.5, -0.5, -0.5,
-
-    // Top face
-    -0.5,  0.5, -0.5,
-    -0.5,  0.5,  0.5,
-     0.5,  0.5,  0.5,
-     0.5,  0.5, -0.5,
-
-    // Bottom face
-    -0.5, -0.5, -0.5,
-     0.5, -0.5, -0.5,
-     0.5, -0.5,  0.5,
-    -0.5, -0.5,  0.5,
-
-    // Left face
-     0.5, -0.5, -0.5,
-     0.5,  0.5, -0.5,
-     0.5,  0.5,  0.5,
-     0.5, -0.5,  0.5,
-
-    // Right face
-    -0.5, -0.5, -0.5,
-    -0.5, -0.5,  0.5,
-    -0.5,  0.5,  0.5,
-    -0.5,  0.5, -0.5
-  ])
-
-  var cubeCount = 2
-
-  // add vetices for each cube
-  var verticesArray = new Float32Array(cube.length * cubeCount)
-  for (var i = 0; i < cubeCount; ++i) {
-    var thisCube = new Float32Array(cube.length)
-    thisCube.set(cube)
-
-    if (i == 1) {
-      // test moving over the 2nd cube as an example, to prove it exists
-      // TODO: real transformations, for each body part
-      var matrix = mat4.create()
-      mat4.scale(matrix, matrix, [1.0, 1.5, 0.5])
-      mat4.translate(matrix, matrix, [0, -1.0, 0]) // TODO: fix 1/8 offset Y
-
-      applyTransformToVertices(thisCube, matrix)
-    }
-
-    verticesArray.set(thisCube, cube.length * i)
-  }
-
-  // Create the position buffer.
-  var vertices = createBuffer(gl, verticesArray)
-
-  var cubeVertexIndices = new Uint16Array([
-    0,  1,  2,      0,  2,  3,    // back
-    4,  5,  6,      4,  6,  7,    // front
-    8,  9,  10,     8,  10, 11,   // top
-    12, 13, 14,     12, 14, 15,   // bottom
-    16, 17, 18,     16, 18, 19,   // left
-    20, 21, 22,     20, 22, 23    // right
-  ])
-
-  // repeat vertex indices for each cube, offset by cube vertex count
-  var indexArray = new Uint16Array(cubeVertexIndices.length * cubeCount)
-  for (var i = 0; i < cubeCount; ++i) {
-    for (var j = 0; j < cubeVertexIndices.length; ++j) {
-      indexArray[i * cubeVertexIndices.length + j] = cubeVertexIndices[j] + (cube.length / 3) * i
-    }
-  }
-
-  // Create the index buffer. This is instead packed into
-  // a UInt16Array: note that this is important, otherwise
-  // your model won't render correctly. Also important is
-  // that you label this buffer as an ELEMENT_ARRAY_BUFFER,
-  // or WebGL will hassle you and refuse to draw the VAO.
-  var index = createBuffer(gl
-    , indexArray
-    , gl.ELEMENT_ARRAY_BUFFER
-  )
-
-
-  var uvArray = new Float32Array(2 * 4 * 6 * cubeCount)
-
-  var tw = 64, th = 32
-  var setCubeFaceUV = function(face,x,y,w,h) {
-    w = w || 8
-    h = h || 8
-
-    var i = face * 8
-
-    uvArray[i    ] =  x      / tw
-    uvArray[i + 1] = (y + h) / th
-
-    uvArray[i + 2] =  x      / tw
-    uvArray[i + 3] =  y      / th
-
-    uvArray[i + 4] = (x + w) / tw
-    uvArray[i + 5] =  y      / th
-
-    uvArray[i + 6] = (x + w) / tw
-    uvArray[i + 7] = (y + h) / th
-  }
-
-  // head
-  setCubeFaceUV(0, 24, 8, 8, 8) // back
-  setCubeFaceUV(1,  8, 8, 8, 8) // front
-  setCubeFaceUV(2,  8, 0, 8, 8) // top
-  setCubeFaceUV(3, 16, 0, 8, 8) // bottom
-  setCubeFaceUV(4,  0, 8, 8, 8) // left // TODO: this has to be rotated?
-  setCubeFaceUV(5, 16, 8, 8, 8) // right
-
-  // TODO: body
-  setCubeFaceUV(6, 24, 8, 8, 8) // back
-  setCubeFaceUV(7,  8, 8, 8, 8) // front
-  setCubeFaceUV(8,  8, 0, 8, 8) // top
-  setCubeFaceUV(9, 16, 0, 8, 8) // bottom
-  setCubeFaceUV(10, 0, 8, 8, 8) // left
-  setCubeFaceUV(11,16, 8, 8, 8) // right
-
-
-  uv = createBuffer(gl, uvArray)
-
-  // Create a VAO from the position buffer, indexed by the
-  // index buffer.
-  mesh = createVAO(gl, [
-      {
-        buffer: vertices
-        , size: 3
-      },
-      {
-        buffer: uv
-        , size: 2
-      }
-  ], index)
-  mesh.length = indexArray.length
-
-  return mesh
 }
 
 var view = new Float32Array(16)
